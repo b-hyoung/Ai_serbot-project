a475948a43e2f573be78399e26cadd3562da0f7f
b-hyoung <youqlrqod@gmail.com>
2025-12-04 04:24:35 +0900

    추가 : 서버 <-> rbot 소켓 송수신

---
+++ backend-server/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.example.service.RobotSocketService;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        RobotSocketService robotSocketService = new RobotSocketService();
+        robotSocketService.startServer();
+        // 로봇이 연결 된 후 보내주기 -> 테스트니까 언제 올지 모름 연결이
+        System.out.println("⏳ 로봇 접속을 기다리는 중...");
+        while (!robotSocketService.isConnected()) {
+            Thread.sleep(2000); // 1초 쉬기
+        }
+
+        // 연결된 후에 전송!
+        System.out.println("✨ 로봇 감지됨! 명령 전송!");
+        robotSocketService.sendToMessage("FORWARD");
+    }
+}
